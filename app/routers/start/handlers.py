49aff176b7defa5f7dee3c100e76b0c54d814f35
petrovleonid <petrovleonid1999@mail.ru>
2025-08-04 19:39:50 +0300

    adding for start_router new handler for F.text

--- app/routers/start/handlers.py
+++ app/routers/start/handlers.py
@@ -4,7 +4,7 @@ from aiogram.types import Message
 from aiogram_dialog import DialogManager
 
 from app.dialogs.uivk_dialog.uivk_dialog_states import UivkDialogStatesGroup
-from aiogram import Router
+from aiogram import Router, F
 
 start_router = Router()
 
@@ -13,4 +13,11 @@ start_router = Router()
 async def start_dialog_command(message: Message, state: FSMContext, dialog_manager: DialogManager):
     await dialog_manager.start(
         UivkDialogStatesGroup.uivk_start_menu
+    )
+
+
+@start_router.message(F.text)
+async def start_dialog_text(message: Message, state: FSMContext, dialog_manager: DialogManager):
+    await dialog_manager.start(
+        UivkDialogStatesGroup.uivk_vacancy_faq
     )
