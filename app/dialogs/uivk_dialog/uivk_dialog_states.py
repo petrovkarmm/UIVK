2ecada6511b2daa918afe860e2f0a2b4343307ef
petrovleonid <petrovleonid1999@mail.ru>
2025-08-04 09:27:36 +0300

    faq answer state

--- app/dialogs/uivk_dialog/uivk_dialog_states.py
+++ app/dialogs/uivk_dialog/uivk_dialog_states.py
@@ -4,3 +4,4 @@ from aiogram.fsm.state import StatesGroup, State
 class UivkDialogStatesGroup(StatesGroup):
     uivk_start_menu = State()
     uivk_vacancy_faq = State()
+    uivk_vacancy_faq_answer = State()
