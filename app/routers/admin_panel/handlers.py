b5481c0860f56d7ca86ea8a59227d242e694cbff
petrovkarmm <petrovleonid1999@mail.ru>
2025-08-14 18:35:38 +0300

    error =/

--- app/routers/admin_panel/handlers.py
+++ app/routers/admin_panel/handlers.py
@@ -12,7 +12,7 @@ admin_panel = Router()
 @admin_panel.message(F.text == 'admin_test')
 async def start_admin_panel_dialog(message: Message, state: FSMContext, dialog_manager: DialogManager):
     try:
-        await dialog_manager.close_manager()
+        await dialog_manager.done()
     except Exception as e:
         bot_logger.warning(e)
     finally:
