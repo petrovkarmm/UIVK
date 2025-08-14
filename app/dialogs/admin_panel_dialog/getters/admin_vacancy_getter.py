4606cc8eb2d2f7b0f71e785cf4c9a044cdaaad99
petrovkarmm <petrovleonid1999@mail.ru>
2025-08-14 16:32:01 +0300

    update admin getter

--- app/dialogs/admin_panel_dialog/getters/admin_vacancy_getter.py
+++ app/dialogs/admin_panel_dialog/getters/admin_vacancy_getter.py
@@ -4,14 +4,17 @@ from app.database.dataclasses.vacancy_dataclass import Vacancy, VACANCY_KEY
 
 
 async def all_admin_vacancy_getter(dialog_manager: DialogManager, **_kwargs):
-    vacancies = Vacancy.get_all()
+    vacancies = Vacancy.get_all(include_hidden=True)
     vacancy_data_flag = bool(vacancies)
 
-    # Форматируем названия
     for vacancy in vacancies:
-        vacancy.vacancy_name = Vacancy.formatted_hidden_vacancy(vacancy.vacancy_name, vacancy.hidden)
+        vacancy.vacancy_name = Vacancy.formatted_hidden_vacancy(
+            vacancy.vacancy_name,
+            vacancy.hidden_status
+        )
 
     return {
         VACANCY_KEY: vacancies,
         "vacancy_data_flag": vacancy_data_flag
     }
+
