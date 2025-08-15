76a151f3f6a95a4a7102098d2db5b940a04730cd
petrovleonid <petrovleonid1999@mail.ru>
2025-08-15 09:54:29 +0300

    rename admin panel states

--- app/dialogs/admin_panel_dialog/admin_dialog_states.py
+++ app/dialogs/admin_panel_dialog/admin_dialog_states.py
@@ -3,5 +3,5 @@ from aiogram.fsm.state import StatesGroup, State
 
 class AdminPanelStatesGroup(StatesGroup):
     admin_panel_menu = State()
-    admin_panel_vacancy = State()
-    admin_panel_faq = State()
+    admin_panel_vacancy_and_questions = State()
+    admin_panel_vacancy_faq_answer = State()
