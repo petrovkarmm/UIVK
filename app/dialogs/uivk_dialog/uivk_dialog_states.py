6d8944a2247fd2b011e3f2bf8995e92e370b5f24
petrovleonid <petrovleonid1999@mail.ru>
2025-08-15 09:55:20 +0300

    rename

--- app/dialogs/uivk_dialog/uivk_dialog_states.py
+++ app/dialogs/uivk_dialog/uivk_dialog_states.py
@@ -3,5 +3,5 @@ from aiogram.fsm.state import StatesGroup, State
 
 class UivkDialogStatesGroup(StatesGroup):
     uivk_start_menu = State()
-    uivk_vacancy_faq = State()
+    uivk_vacancy_and_questions = State()
     uivk_vacancy_faq_answer = State()
