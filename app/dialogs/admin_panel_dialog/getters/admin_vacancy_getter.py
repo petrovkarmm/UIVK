44a35e62299cbd1d05fd9ab4ca119f860991043e
petrovleonid <petrovleonid1999@mail.ru>
2025-08-19 09:48:47 +0300

    current vacancy getter

--- app/dialogs/admin_panel_dialog/getters/admin_vacancy_getter.py
+++ app/dialogs/admin_panel_dialog/getters/admin_vacancy_getter.py
@@ -19,5 +19,26 @@ async def all_admin_vacancy_getter(dialog_manager: DialogManager, **_kwargs):
     }
 
 
-async def admin_current_vacancy_getter():
-    pass # TODO добавить код на геттер для удаления
+async def admin_current_vacancy_getter(dialog_manager: DialogManager, **_kwargs):
+    vacancy_id = dialog_manager.dialog_data['vacancy_id']
+    vacancy_data = Vacancy.get_by_id(vacancy_id)
+
+    vacancy_name = vacancy_data.vacancy_name
+    vacancy_hidden_status = vacancy_data.hidden_status
+    vacancy_created = vacancy_data.created
+    vacancy_updated = vacancy_data.updated
+
+    print(vacancy_updated)
+    print(vacancy_created)
+
+    if vacancy_hidden_status:
+        vacancy_status = "скрыта"
+    else:
+        vacancy_status = "открыта"
+
+    return {
+        'vacancy_name': vacancy_name,
+        'vacancy_status': vacancy_status,
+        'vacancy_created': vacancy_created,
+        'vacancy_updated': vacancy_updated
+    }
